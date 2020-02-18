4348b0fcbb9a3d8414e709b9deac4a2ab17c7237
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-02-18 12:24:04 -0800

    commit to add user validation using the push method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,11 @@
+/*these codes are also connected to the router and
+and calls the appropriate model and view 
+The job of the contoller here is to accept input 
+and convert these inputs into the apropriate commands
+for model and view*/
+
+const User = require('../models/User')
+
 exports.login = function(){
 
 }
@@ -7,10 +15,19 @@ exports.logout= function(){
 }
 
 exports.register= function(req, res){
-res.send("Thanks for trying to register")
 
+    let user = new User(req.body)
+
+user.register()
+if(user.errors.length){
+
+res.send(user.errors)
+
+}else {
+res.send("Congrats, there are no errors")
 }
-
+}
+ 
 exports.home= function(req, res){
     res.render('home-guest')
 }
--- router.js
+++ router.js
@@ -1,8 +1,15 @@
+/* the purpose of this router is to list out the urls the app
+ should listen to and say what should happen for each f these routes*/
+
+ //declare variables as const to prevent them from being changed
 const express = require ('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
 
+
 router.get('/', userController.home)
+
+//send a post request to the server 
 router.post('/register', userController.register )
 
 module.exports = router 

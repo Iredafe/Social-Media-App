9bb60795c574b857abeb2ddd145c6b918a6f052d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-10 14:52:30 -0700

    still fixing login

--- models/User.js
+++ models/User.js
@@ -50,14 +50,16 @@ User.prototype.validate = function(){
 User.prototype.login = function(){
     return new Promise((resolve, reject)=>{
         this.cleanUp()
-        userCollection.findOne({username: this.data.username}).then(()=>{
+        userCollection.findOne({username: this.data.username}).then((attemptedUser)=>{
             if (attemptedUser && attemptedUser.password == this.data.password){
                 resolve("Congrats!!!!")
             }else{
             reject("Invalid username or password!")
             }
         
-        }).catch()
+        }).catch(function(){
+            reject("Please try again later!")
+        })
     
     })
   }
